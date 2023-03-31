62875878550491adf6d0d5be1a7cc6256a5341b6
Daniel Araujo <danielaraujo.github@gmail.com>
2023-03-30 22:40:06 -0300

    commit 4

---
+++ ex8.py
@@ -0,0 +1,9 @@
+# 8) Escreva um programa que determine o número de dias e de horas que uma pessoa já viveu.
+# Considere que um mês tem 30 dias.
+
+anoDeHoje = int(input("Em que ano estamos? R: "))
+anoDeNasc = int(input("Em que ano você nasceu? R: "))
+
+idadeDoUser = anoDeHoje - anoDeNasc
+
+diasVividos = idadeDoUser * 12) * 30)
