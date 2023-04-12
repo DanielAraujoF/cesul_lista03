4794436789e2428f46aa107a217fa7b1b8518847
Daniel Araujo <danielaraujo.github@gmail.com>
2023-04-12 19:18:06 -0300

    commit 4

--- ex8.py
+++ ex8.py
@@ -2,8 +2,15 @@
 # Considere que um mês tem 30 dias.
 
 anoDeHoje = int(input("Em que ano estamos? R: "))
+mesDeHoje = int(input("Em que mês estamos? R: "))
+diaDeHoje = int(input("Em que dia estamos? R: "))
+
 anoDeNasc = int(input("Em que ano você nasceu? R: "))
+mesDeNasc = int(input("Em que mês você nasceu? R: "))
+diaDeNasc = int(input("Em que dia você nasceu? R: "))
 
 idadeDoUser = anoDeHoje - anoDeNasc
+
+
 
 diasVividos = idadeDoUser * 12) * 30)
